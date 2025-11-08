516a070f923f3b3eb909eecb96069e71c5fc12a9
mr_tomate <mr_tomate2024@hotmail.com>
2025-11-08 19:18:38 -0400

    Update API base URL
    The base URL for API requests has been updated to the new local network
    IP address.

--- lib/api.ts
+++ lib/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import * as SecureStore from "expo-secure-store";
 
 const api = axios.create({
-  baseURL: "http://localhost:300/api",
+  baseURL: "http://192.168.1.18:3000/api",
 });
 
 api.interceptors.request.use(async (config) => {
